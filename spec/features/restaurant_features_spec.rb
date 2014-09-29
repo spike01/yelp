3739d8f1b4d74124f802ad36e55d38eda3f3ffb8
Spike Nox <spike01@gmail.com>
2014-09-29 14:16:45 +0100

    Adds conditional display of restaaaauauuuuuurraaaants

--- spec/features/restaurant_features_spec.rb
+++ spec/features/restaurant_features_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 describe 'Restaurants' do
-  context 'No restaurant has been added' do
+  context 'No restaurants have been added' do
     it 'displays a prompt to add restaurants' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants found'
       expect(page).to have_link 'Add a restaurant'
+    end
+  end
+
+  context 'Added a restaurant' do
+    before do
+      Restaurant.create(name: 'Nobu')
+      end
+    it 'displays a restaurant when added' do
+      visit '/restaurants'
+      expect(page).to have_content 'Nobu'
+      expect(page).not_to have_content 'No restaurants found'
     end
   end
 end
